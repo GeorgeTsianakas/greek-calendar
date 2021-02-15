7c672377d904a3503cd075102010a992dff4648e
George <gtsianakas94@gmail.com>
2021-02-15 14:48:14 +0200

    Update AllGreekPublicHolidaysTests.java
    Update AllGreekPublicHolidaysTests.java file

--- src/test/java/AllGreekPublicHolidaysTests.java
+++ src/test/java/AllGreekPublicHolidaysTests.java
@@ -1,35 +1,34 @@
 import calendar.HolidayCalculator;
 import calendar.Holidays;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import java.time.LocalDate;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class AllGreekPublicHolidaysTests {
+    public AllGreekPublicHolidaysTests() {
+    }
 
     @Test
     public void VerifyDates() {
-
         Holidays holidays = HolidayCalculator.CreateHolidays(2021);
-
         List<LocalDate> list = holidays.getAllGreekPublicHolidays();
-
-        assertTrue(list.contains(LocalDate.of(2021, 1, 1)));//New Year's Day
-        assertTrue(list.contains(LocalDate.of(2021, 1, 6)));//Epiphany
-        assertTrue(list.contains(LocalDate.of(2021, 3, 15)));//Orthodox Ash Monday
-        assertTrue(list.contains(LocalDate.of(2021, 3, 25)));//Independence Day
-        assertTrue(list.contains(LocalDate.of(2021, 4, 30)));//Orthodox Good Friday
-        assertTrue(list.contains(LocalDate.of(2021, 5, 1)));//Labour Day
-        assertTrue(list.contains(LocalDate.of(2021, 5, 2)));//Orthodox Easter Sunday
-        assertTrue(list.contains(LocalDate.of(2021, 5, 3)));//Orthodox Easter Monday
-        assertTrue(list.contains(LocalDate.of(2021, 6, 21)));//Orthodox Whit Monday
-        assertTrue(list.contains(LocalDate.of(2021, 8, 15)));//Assumption Day
-        assertTrue(list.contains(LocalDate.of(2021, 10, 28)));//Ochi Day
-        assertTrue(list.contains(LocalDate.of(2021, 12, 25)));//Christmas Day
-        assertTrue(list.contains(LocalDate.of(2021, 12, 26)));//2nd Day of Christmas
+        assertTrue(list.contains(LocalDate.of(2021, 1, 1)));
+        assertTrue(list.contains(LocalDate.of(2021, 1, 6)));
+        assertTrue(list.contains(LocalDate.of(2021, 3, 15)));
+        assertTrue(list.contains(LocalDate.of(2021, 3, 25)));
+        assertTrue(list.contains(LocalDate.of(2021, 4, 30)));
+        assertTrue(list.contains(LocalDate.of(2021, 5, 1)));
+        assertTrue(list.contains(LocalDate.of(2021, 5, 2)));
+        assertTrue(list.contains(LocalDate.of(2021, 5, 3)));
+        assertTrue(list.contains(LocalDate.of(2021, 6, 21)));
+        assertTrue(list.contains(LocalDate.of(2021, 8, 15)));
+        assertTrue(list.contains(LocalDate.of(2021, 10, 28)));
+        assertTrue(list.contains(LocalDate.of(2021, 12, 25)));
+        assertTrue(list.contains(LocalDate.of(2021, 12, 26)));
 
         assertEquals(13, list.size());
     }
@@ -37,11 +36,10 @@ public class AllGreekPublicHolidaysTests {
     @Test
     public void VerifyNoDuplicates() {
 
-        Holidays holidays = HolidayCalculator.CreateHolidays(1994);  // May first on easter day.
-
+        Holidays holidays = HolidayCalculator.CreateHolidays(1994);
         List<LocalDate> list = holidays.getAllGreekPublicHolidays();
-
         assertEquals(12, list.size());
+
     }
 
 }
